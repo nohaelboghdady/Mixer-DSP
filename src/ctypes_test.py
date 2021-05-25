b9c4653ca9e31139ac175180a99374a59eb91608
Kamel <Mohammed99kamel@gmail.com>
2021-05-25 22:36:16 +0200

    Part B

--- src/ctypes_test.py
+++ src/ctypes_test.py
@@ -1,13 +1,12 @@
-from ctypes import c_double, c_int
+from ctypes import c_double, c_int, CDLL
 import numpy as np
 import time
 import matplotlib.pyplot as plt
 from numpy.ctypeslib import ndpointer
 import ctypes
-import os
 
 # loading the library
-c_lib = ctypes.CDLL("./test.dll")
+c_lib = CDLL("/Users/moham/Desktop/Mixer-DSP/src/test2.dll")
 
 ft = c_lib.dft
 fft=c_lib.fft
@@ -16,26 +15,24 @@ ft.argtypes = [ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctype
 
 fft.argtypes =[ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),ctypes.c_int]
 
-signal_length = []
+signal_length=[1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192]
 execution_time_dft = []
 execution_time_fft = []
 error = []
-count = 0
-while count < 1000:
-    count += 10
+for i in signal_length:
     #filling input array
-    signal_length.append(count)
-    main_input = np.arange(count)
+    main_input = np.arange(i)
     samples = len(main_input)
     
     #preparing arguments
     zero =np.zeros(samples)
     dft_input = np.column_stack((main_input, zero))
+    dft_output= np.column_stack((main_input, zero))
     fft_input = np.column_stack((main_input, zero))
     
     #dft
     start1 = time.time()
-    ft(dft_input, samples)
+    ft(dft_input, dft_output, samples)
     end1 = time.time()
     execution_time_dft.append(end1-start1)
     
@@ -46,22 +43,40 @@ while count < 1000:
     execution_time_fft.append(end2-start2)
     
     #calc error
-    mean_error= np.square(np.subtract(dft_input, fft_input)).mean()
+    # mean_error= np.subtract(dft_output, fft_input)
+    mean_error= np.square(np.subtract(dft_output, fft_input)).mean()
+    
     error.append(mean_error)
 
+
 #plotting Complexities
-plt.subplot(211)
-plt.plot(signal_length, execution_time_dft)
+
+
+
+plt.subplot(221)
 plt.ylabel('execution_time')
+plt.xlabel('signal_length')
+plt.plot(signal_length, execution_time_dft)
+plt.plot(signal_length,execution_time_fft)
+plt.subplot(222)
+plt.xlabel('signal_length')
+plt.ylabel('execution_time')
+plt.title("DFT")
+plt.plot(signal_length, execution_time_dft)
+plt.subplot(223)
+plt.xlabel('signal_length')
+plt.ylabel('execution_time')
+plt.title("FFT")
+plt.ylim(0,0.2)
 plt.plot(signal_length,execution_time_fft)
 
 
 #plotting errors
-plt.subplot(212)
+plt.subplot(224)
 plt.plot(signal_length,error)
 plt.xlabel('signal_length')
 plt.ylabel('Error')
-
+plt.title("Error")
 plt.show()
 
 
--- src/test.cpp
+++ src/test.cpp
@@ -1,44 +1,51 @@
 #include<complex>
+#include<iostream>
 using namespace std;
-#define DLLEXPORT extern "C" __declspec(dllexport)
 
 extern "C"
 {
- DLLEXPORT void fft(complex<double> *x, const int N) {
-	if (N <= 1) {
-		return;
+
+void fft(std::complex<double> input_output_signal[] , int size)
+{
+	const size_t N = size;
+	if (N <= 1){
+		return;}
+	int M = size/2;
+	
+	std::complex<double>* even = new std::complex<double>[M];
+	std::complex<double>* odd = new std::complex<double>[M];
+
+	for(int i = 0; i < M; i++)
+	{
+		even[i] = input_output_signal[2*i];
+		odd [i] = input_output_signal[2*i + 1];
 	}
 
-	    complex<double> odd[N/2];
-	    complex<double> even[N/2];
-	for (int i = 0; i < N / 2; i++) {
-		even[i] = x[i*2];
-		odd[i] = x[i*2+1];
-	}
+	// conquer
+	fft(even, M);
+	fft(odd, M);
 
-	fft(even, N/2);
-	fft(odd, N/2);
-
-
-	for (int k = 0; k < N / 2; k++) {
-		complex<double> t = exp(complex<double>(0, -2 * M_PI * k / N)) *odd[k];
-        x[k] = even[k] + t;
-		x[N / 2 + k] = even[k] - t;
+	// combine
+	for (size_t k = 0; k < N/2; ++k)
+	{
+		std::complex<double> t = std::polar(1.0, -2 * M_PI * k / N) * odd[k];
+		input_output_signal[k] = even[k] + t;
+		input_output_signal[k+N/2] = even[k] - t;
 	}
 }
 
-DLLEXPORT void dft ( complex<double>sig_data[],complex<double>output[], int N)
+void dft ( complex<double>sig_data[],complex<double>output[], int N)
 {
      complex<double> intSum;
-
-    for ( int k=0; k<N; k++)
+   for ( int k=0; k<N; k++)
     {
         intSum=  complex<double>(0,0);
         for ( int n=0; n<N; n++)
         {
-            double real = cos (((2*M_PI)/N) *k * n);
-            double imaginary = sin (((2*M_PI)/N) *k * n);
-             complex<double> complex_fn  (real, -imaginary);
+			double angle = 2 * M_PI * n * k / N;
+            double real = cos (angle);
+            double imaginary = sin (angle);
+            complex<double> complex_fn  (real, -imaginary);
             intSum += sig_data[n] *complex_fn;
         }
         output[k]= intSum;
